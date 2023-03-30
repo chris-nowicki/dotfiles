b175d26d30f9e2e6a4a496abb4a1025b06429727
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-03-30 15:42:26 -0400

    organized files into folders and updated zshrc  and install config files

---
+++ brew/generate_brewfile.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Generating Brewfile >>>"
+
+brew bundle dump --force --describe > ~/.dotfiles/brew/Brewfile
+python3 ~/.dotfiles/brew/parse_brewfile.py
+mv ~/.dotfiles/Brewfile ~/.dotfiles/brew/
+echo "*** Brewfile Generated ***\n"
---
+++ zsh/aliases.zsh
@@ -0,0 +1,4 @@
+alias ls='exa -laFh --git'
+alias exa='exa -laFh --git'
+alias bbd="./brew/generate_brewfile.zsh"
+alias trail="<<<${(F)path}"
