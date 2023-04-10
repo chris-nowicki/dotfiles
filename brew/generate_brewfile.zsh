9afffdaf80564d9b0bc0ef902b99eb111c9378ab
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-04-10 14:17:26 -0400

    added yarn and obsidian to Brewfile and updated generate_brewfile scripts to make sure file is dumped in ~/.dotfiles/brew/

--- brew/generate_brewfile.zsh
+++ brew/generate_brewfile.zsh
@@ -2,7 +2,6 @@
 
 echo "\n<<< Generating Brewfile >>>"
 
-brew bundle dump --force --describe > ~/.dotfiles/brew/Brewfile
+brew bundle dump --force --describe --file=~/.dotfiles/brew/Brewfile
 python3 ~/.dotfiles/brew/parse_brewfile.py
-mv ~/.dotfiles/Brewfile ~/.dotfiles/brew/
 echo "*** Brewfile Generated ***\n"
--- brew/parse_brewfile.py
+++ brew/parse_brewfile.py
@@ -1,7 +1,9 @@
-with open("Brewfile", "r") as file:
+import os
+
+with open(os.path.expanduser("~/.dotfiles/brew/Brewfile"), "r") as file:
     lines = file.readlines()
 
-with open("Brewfile", "w") as file:
+with open(os.path.expanduser("~/.dotfiles/brew/Brewfile"), "w") as file:
     for line in lines:
         if line.startswith("cask"):
             line = line.strip() + ", args: { no_quarantine: true }\n"
