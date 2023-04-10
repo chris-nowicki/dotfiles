17cc49d099f221cfb64d16e4c1e52f645d87672d
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-04-10 14:42:29 -0400

    fixed the trail alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias ls='exa -laFh --git'
 alias exa='exa -laFh --git'
 alias bbd="~/.dotfiles/brew/generate_brewfile.zsh > ~/.dotfiles/brew/"
-alias trail="<<<${(F)path}"
+alias trail='<<<${(F)path}'
