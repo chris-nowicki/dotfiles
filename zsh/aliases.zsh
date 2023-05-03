60c6d125dc1c23def226e76a596ba8bc92642fe9
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-05-03 08:21:53 -0400

    updated brewfile dump alias in zsh and starship config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias ls='exa -laFh --git'
 alias exa='exa -laFh --git'
-alias bbd="~/.dotfiles/brew/generate_brewfile.zsh > ~/.dotfiles/brew/"
+alias bbd="~/.dotfiles/brew/generate_brewfile.zsh > ~/.dotfiles/brew/Brewfile"
 alias trail='<<<${(F)path}'
--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -2,5 +2,6 @@
 # Syntax highlighting for man pages using bat
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export HOMEBREW_CASK_OPTS="--no-quarantine"
+export NVM_DIR="$HOME/.nvm"
 NULLCMD=bat
 skip_global_compinit=1
