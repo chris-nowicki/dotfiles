a705a9aee525377be3f80668abccd6e6af14ee62
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-02-11 14:41:25 -0500

    Remove un-needed dotfiles, add dracula theme submodule, update to oh-my-zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias exa='exa -laFh --git --icons --color=always'
 alias xl='exa -lag --icons --color=always' 
 alias bbd="~/.dotfiles/brew/generate_brewfile.zsh"
 alias trail='<<<${(F)path}'
+alias svgme="svgo *.svg --config=../Users/$(whoami)/.svgo-config.json"
