b03ebf5ae72d342d4c83ca31ec70fc2b6ec89c44
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-04 11:08:22 -0400

    Simplify dotfiles

--- zsh/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias ls='exa -laFh --git --icons --color=always'
-alias exa='exa -laFh --git --icons --color=always'
-alias xl='exa -lag --icons --color=always' 
-alias bbd="~/.dotfiles/brew/generate_brewfile.zsh"
-alias trail='<<<${(F)path}'
-alias svgme="svgo *.svg --config=../Users/$(whoami)/.svgo-config.json"
-alias cm="npx cz"
--- zsh/variables.zsh
+++
@@ -1,7 +0,0 @@
-# Set Variables
-# Syntax highlighting for man pages using bat
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-export HOMEBREW_CASK_OPTS="--no-quarantine"
-export NVM_DIR="$HOME/.nvm"
-NULLCMD=bat
-skip_global_compinit=1
