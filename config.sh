8cf9c442eda63a12227863de2ad1b72db454f720
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-16 16:08:00 -0400

    added cspell custom dictionary link

--- config.sh
+++ config.sh
@@ -6,3 +6,8 @@ for dotfile in $(echo ${DOTFILES[*]});
 do
     cp -f ~/Dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
 done
+
+# Link custom dictionary to cspell
+mkdir -p ~/.cspell && ln -s ~/Dotfiles/custom-dictionary-user.txt ~/.cspell/custom-dictionary-user.txt 2>/dev/null
+
+echo "dotfile config complete!"
