3bd3d511b7ae5b2c563f1b6dc1c610db7117ab1f
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-07-15 10:31:21 -0400

    feature: update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/zsh
 
+# echo "Installing dotfiles..."
+echo "Installing dotfiles..."
+
 for dir in */; do
   stow "$dir"
 done
+
+# check for .hushlogin file and if it does not exist, create it
+echo "Checking for .hushlogin file..."
+
+if [ ! -f ~/.hushlogin ]; then
+  echo "Creating .hushlogin file..."
+  touch ~/.hushlogin
+fi
+
+echo "Done!"
